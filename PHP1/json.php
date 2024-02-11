2547c68fa44b00b4fe7c55de0f8b20ae14aeab9e
JMOM3112 <127054350+JMOM3112@users.noreply.github.com>
2024-02-11 11:08:40 -0600

    Add files via upload

---
+++ PHP1/json.php
@@ -0,0 +1,206 @@
+<?php
+$libros = [];
+
+$libros[] = [
+    "Nombre" => "1984",
+    "Autor" => "George Orwell",
+    "Año" => 1949,
+    "NumeroPaginas" => 328,
+    "ISBN" => "978-0451524935",
+    "Capitulos" => [
+        "Capitulo1" => "La campana de la victoria",
+        "PaginasCap1" => 50,
+        "Capitulo2" => "El ojo de Dios",
+        "PaginasCap2" => 75,
+        "Capitulo3" => "El arresto",
+        "PaginasCap3" => 75,
+        "Capitulo4" => "La habitación 101",
+        "PaginasCap4" => 75,
+        "Capitulo5" => "La caída del Gran Hermano",
+        "PaginasCap5" => 53
+    ]
+];
+
+$libros[] = [
+    "Nombre" => "Cien años de soledad",
+    "Autor" => "Gabriel García Márquez",
+    "Año" => 1967,
+    "NumeroPaginas" => 417,
+    "ISBN" => "978-0307350484",
+    "Capitulos" => [
+        "Capitulo1" => "Melquíades",
+        "PaginasCap1" => 45,
+        "Capitulo2" => "Los Buendía",
+        "PaginasCap2" => 80,
+        "Capitulo3" => "La llegada de Aureliano",
+        "PaginasCap3" => 60,
+        "Capitulo4" => "La soledad de Amaranta Úrsula",
+        "PaginasCap4" => 75,
+        "Capitulo5" => "El último Buendía",
+        "PaginasCap5" => 65
+    ]
+];
+
+$libros[] = [
+    "Nombre" => "To Kill a Mockingbird",
+    "Autor" => "Harper Lee",
+    "Año" => 1960,
+    "NumeroPaginas" => 324,
+    "ISBN" => "978-0061120084",
+    "Capitulos" => [
+        "Capitulo1" => "Maycomb, Alabama",
+        "PaginasCap1" => 30,
+        "Capitulo2" => "Atticus Finch",
+        "PaginasCap2" => 50,
+        "Capitulo3" => "Boo Radley",
+        "PaginasCap3" => 40,
+        "Capitulo4" => "Tom Robinson's Trial",
+        "PaginasCap4" => 75,
+        "Capitulo5" => "The Verdict",
+        "PaginasCap5" => 55
+    ]
+];
+
+$libros[] = [
+    "Nombre" => "The Great Gatsby",
+    "Autor" => "F. Scott Fitzgerald",
+    "Año" => 1925,
+    "NumeroPaginas" => 180,
+    "ISBN" => "978-0743273565",
+    "Capitulos" => [
+        "Capitulo1" => "Nick Carraway",
+        "PaginasCap1" => 20,
+        "Capitulo2" => "Jay Gatsby",
+        "PaginasCap2" => 40,
+        "Capitulo3" => "Daisy Buchanan",
+        "PaginasCap3" => 30,
+        "Capitulo4" => "Tom Buchanan",
+        "PaginasCap4" => 45,
+        "Capitulo5" => "Tragedy Strikes",
+        "PaginasCap5" => 45
+    ]
+];
+
+$libros[] = [
+    "Nombre" => "One Hundred Years of Solitude",
+    "Autor" => "Gabriel García Márquez",
+    "Año" => 1967,
+    "NumeroPaginas" => 417,
+    "ISBN" => "978-0307350484",
+    "Capitulos" => [
+        "Capitulo1" => "Melquíades",
+        "PaginasCap1" => 45,
+        "Capitulo2" => "Los Buendía",
+        "PaginasCap2" => 80,
+        "Capitulo3" => "La llegada de Aureliano",
+        "PaginasCap3" => 60,
+        "Capitulo4" => "La soledad de Amaranta Úrsula",
+        "PaginasCap4" => 75,
+        "Capitulo5" => "El último Buendía",
+        "PaginasCap5" => 65
+    ]
+];
+
+$libros[] = [
+    "Nombre" => "Brave New World",
+    "Autor" => "Aldous Huxley",
+    "Año" => 1932,
+    "NumeroPaginas" => 288,
+    "ISBN" => "978-0060850524",
+    "Capitulos" => [
+        "Capitulo1" => "The Central London Hatchery",
+        "PaginasCap1" => 35,
+        "Capitulo2" => "Lenina Crowne",
+        "PaginasCap2" => 55,
+        "Capitulo3" => "John 'the Savage'",
+        "PaginasCap3" => 40,
+        "Capitulo4" => "The Savage's Rebellion",
+        "PaginasCap4" => 80,
+        "Capitulo5" => "The Fall of the World State",
+        "PaginasCap5" => 78
+    ]
+];
+
+$libros[] = [
+    "Nombre" => "The Catcher in the Rye",
+    "Autor" => "J.D. Salinger",
+    "Año" => 1951,
+    "NumeroPaginas" => 224,
+    "ISBN" => "978-0316769480",
+    "Capitulos" => [
+        "Capitulo1" => "Holden Caulfield",
+        "PaginasCap1" => 30,
+        "Capitulo2" => "Pencey Prep",
+        "PaginasCap2" => 50,
+        "Capitulo3" => "Phoebe",
+        "PaginasCap3" => 40,
+        "Capitulo4" => "The Carousel",
+        "PaginasCap4" => 60,
+        "Capitulo5" => "The Catcher in the Rye",
+        "PaginasCap5" => 44
+    ]
+];
+
+$libros[] = [
+    "Nombre" => "Lord of the Flies",
+    "Autor" => "William Golding",
+    "Año" => 1954,
+    "NumeroPaginas" => 224,
+    "ISBN" => "978-0399501487",
+    "Capitulos" => [
+        "Capitulo1" => "The Crash",
+        "PaginasCap1" => 30,
+        "Capitulo2" => "The Island",
+        "PaginasCap2" => 50,
+        "Capitulo3" => "The Beast",
+        "PaginasCap3" => 40,
+        "Capitulo4" => "Savagery Takes Over",
+        "PaginasCap4" => 60,
+        "Capitulo5" => "Rescue",
+        "PaginasCap5" => 44
+    ]
+];
+
+$libros[] = [
+    "Nombre" => "The Lord of the Rings",
+    "Autor" => "J.R.R. Tolkien",
+    "Año" => 1954,
+    "NumeroPaginas" => 1178,
+    "ISBN" => "978-0544003415",
+    "Capitulos" => [
+        "Capitulo1" => "The Fellowship of the Ring",
+        "PaginasCap1" => 200,
+        "Capitulo2" => "The Two Towers",
+        "PaginasCap2" => 300,
+        "Capitulo3" => "The Return of the King",
+        "PaginasCap3" => 400,
+        "Capitulo4" => "The Destruction of the Ring",
+        "PaginasCap4" => 200,
+        "Capitulo5" => "The Grey Havens",
+        "PaginasCap5" => 78
+    ]
+];
+
+$libros[] = [
+    "Nombre" => "The Hobbit",
+    "Autor" => "J.R.R. Tolkien",
+    "Año" => 1937,
+    "NumeroPaginas" => 310,
+    "ISBN" => "978-0345534835",
+    "Capitulos" => [
+        "Capitulo1" => "An Unexpected Party",
+        "PaginasCap1" => 30,
+        "Capitulo2" => "Riddles in the Dark",
+        "PaginasCap2" => 50,
+        "Capitulo3" => "Battle of the Five Armies",
+        "PaginasCap3" => 40,
+        "Capitulo4" => "Return Journey",
+        "PaginasCap4" => 60,
+        "Capitulo5" => "The Last Stage",
+        "PaginasCap5" => 44
+    ]
+];
+
+echo json_encode($libros, JSON_UNESCAPED_UNICODE);
+
+?>
